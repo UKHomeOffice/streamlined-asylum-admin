2ed4ee47633328a26097f9918ed28f9ba138b661
Vivek Kumar <Vivek.Kumar1@digital.homeoffice.gov.uk>
2026-09-21 10:36:39 +0100

    SAA-57 & SAA-59 send evidence to help your claim pages (#12)

--- apps/saa/fields/index.js
+++ apps/saa/fields/index.js
@@ -1,1 +1,8 @@
-module.exports = {};
+module.exports = {
+  'send-evidence-for-your-claim': {
+    mixin: 'radio-group',
+    options: ['yes', 'no'],
+    validate: ['required'],
+    isPageHeading: true
+  }
+};
